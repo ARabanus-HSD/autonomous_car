14c6ba905c22c6b1a6487a67877b128f042e3ebe
AntonRabanusvGreens <anton.rabanus@v-greens.com>
2024-01-13 16:53:45 +0100

    added variables

--- car_control.py
+++ car_control.py
@@ -3,7 +3,7 @@
 from machine import Pin
 import time
 
-def motor(direction=["w", "s"]):
+def motor(direction=["w", "s", "x"]):
     """
     input is w or s as a string
     returns bool:
@@ -11,12 +11,14 @@ def motor(direction=["w", "s"]):
         false for backwards
     """
     if direction == "w":
-        engine_status = True
+        engine_status = 1
     elif direction == "s":
-        engine_status = False
+        engine_status = -1
+    else:
+        engine_status = 0
     return engine_status 
 
-def steering(direction=["a", "d"]):
+def steering(direction=["a", "d", "x"]):
     """
     input is a or d as a string
     returns bool:
@@ -24,10 +26,14 @@ def steering(direction=["a", "d"]):
         false for right
     """
     if direction == "a":
-        turning = True
+        turning = 1
     elif direction == "d":
-        turning = False
+        turning = -1
+    else:
+        turning = 0
     return turning
+
+
 
 pin_fw = Pin(3, mode=Pin.OUT)
 pin_fw = Pin(5, mode=Pin.OUT)
@@ -36,24 +42,58 @@ pin_r = Pin(11, mode=Pin.OUT)
 
 while True:
 
-    f_b = input("forward or backward?\n")
+    # collect use input on where you want to go
+    f_b = input("forward (w) or backward (s) or none (x)?\n")
+    l_r = input("left (a) or right (d) or none (x)?\n")
 
-    if motor(f_b) is True:
-        print("going forwards\n")
-        pin_fw = Pin(16, mode=Pin.OUT, value=1)
-        time.sleep(1)
-    elif motor(f_b) is False:
-        print("going backwards\n")
-        pin_fw = Pin(16, mode=Pin.OUT, value=1)
-        time.sleep(1)
 
-    l_r = input("left or right?\n")
-
-    if steering(l_r) is True:
+    # check if steering is left
+    if steering(l_r) == 1:
         print("going left\n")
         pin_l = Pin(16, mode=Pin.OUT, value=1)
-        time.sleep(1)
-    elif steering(l_r) is False:
+        # check if forwards, backwards or none
+        if motor(f_b) == 1:
+            print("going forwards\n")
+            pin_fw = Pin(16, mode=Pin.OUT, value=1)
+            time.sleep(1)
+        elif motor(f_b) == -1:
+            print("going backwards\n")
+            pin_fw = Pin(16, mode=Pin.OUT, value=1)
+            time.sleep(1)
+        elif motor(f_b) == 0:
+            print("doing nothing")
+            time.sleep(1)
+
+    # check if steering is right
+    elif steering(l_r) == -1:
         print("going right\n")
         pin_r = Pin(16, mode=Pin.OUT, value=1)
-        time.sleep(1)
+        # check if forwards, backwards or none
+        if motor(f_b) == 1:
+            print("going forwards\n")
+            pin_fw = Pin(16, mode=Pin.OUT, value=1)
+            time.sleep(1)
+        elif motor(f_b) == -1:
+            print("going backwards\n")
+            pin_fw = Pin(16, mode=Pin.OUT, value=1)
+            time.sleep(1)
+        elif motor(f_b) == 0:
+            print("doing nothing")
+            time.sleep(1)
+
+    # check if steering is none
+    elif steering(l_r) == 0:
+        print("going right\n")
+        pin_r = Pin(16, mode=Pin.OUT, value=1)
+        # check if forwards, backwards or none
+        if motor(f_b) == 1:
+            print("going forwards\n")
+            pin_fw = Pin(16, mode=Pin.OUT, value=1)
+            time.sleep(1)
+        elif motor(f_b) == -1:
+            print("going backwards\n")
+            pin_fw = Pin(16, mode=Pin.OUT, value=1)
+            time.sleep(1)
+        elif motor(f_b) == 0:
+            print("doing nothing")
+            time.sleep(1)
